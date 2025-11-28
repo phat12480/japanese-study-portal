2af5fb3a68bbcfca809637ac5d6ea96fb07f0975
phat12480 <tanphat014339@gmail.com>
2025-11-28 20:06:59 +0700

    manage vocab

--- backend/controllers/vocabController.js
+++ backend/controllers/vocabController.js
@@ -1,5 +1,6 @@
 const Vocabulary = require("../models/Vocabulary");
 
+// GET ALL
 exports.getAllVocabs = async (req, res) => {
   try {
     const data = await Vocabulary.find();
@@ -9,11 +10,34 @@ exports.getAllVocabs = async (req, res) => {
   }
 };
 
+// ADD NEW
 exports.addVocab = async (req, res) => {
   try {
     const vocab = await Vocabulary.create(req.body);
     res.json(vocab);
   } catch (err) {
     res.status(500).json({ msg: "Error adding vocabulary" });
+  }
+};
+
+// UPDATE
+exports.updateVocab = async (req, res) => {
+  try {
+    const vocab = await Vocabulary.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(vocab);
+  } catch (err) {
+    res.status(500).json({ msg: "Error updating vocabulary" });
+  }
+};
+
+// DELETE
+exports.deleteVocab = async (req, res) => {
+  try {
+    await Vocabulary.findByIdAndDelete(req.params.id);
+    res.json({ msg: "Deleted" });
+  } catch (err) {
+    res.status(500).json({ msg: "Error deleting vocabulary" });
   }
 };
--- backend/routes/vocabRoutes.js
+++ backend/routes/vocabRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getAllVocabs, addVocab } = require("../controllers/vocabController");
+
+const {
+  getAllVocabs,
+  addVocab,
+  updateVocab,
+  deleteVocab,
+} = require("../controllers/vocabController");
 
 router.get("/", getAllVocabs);
 router.post("/", addVocab);
+router.put("/:id", updateVocab);
+router.delete("/:id", deleteVocab);
 
 module.exports = router;
